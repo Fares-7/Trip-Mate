f5493e2c3f7608e06c708242fc3ff0507f1bd4a0
fares <faresghandour718@gmail.com>
2024-10-01 13:48:21 +0300

    adding the table

--- resources/views/front/myreservation.blade.php
+++ resources/views/front/myreservation.blade.php
@@ -1,3 +1,31 @@
 @extends('front.master')
 @section('title', 'My Reservations')
 @include('front.partials.hero', ['title' => 'My Reservations'])
+@section('content')
+    <table class="table">
+        <caption>My Reservations</caption>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Destination</th>
+                <th scope="col">Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <th>1</th>
+            <td>test</td>
+            <td>test</td>
+            {{-- @if (count($data) > 0)
+                @foreach ($data as $key => $user)
+                    <tr>
+                        <th scope="row">{{ $data->firstItem() + $loop->index }}</th>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            @endif --}}
+        </tbody>
+        {{-- {{ $data->links() }} --}}
+        {{-- {{ $data->render('pagination::bootstrap-5') }} --}}
+    </table>
+@endsection
